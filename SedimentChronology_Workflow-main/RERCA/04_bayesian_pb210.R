9b9aaa023f41bb5ffb19e74bac4e1eb136233cc2
CathalD <cathal.d@hotmail.com>
2026-05-29 12:16:14 -0400

    Update 04_bayesian_pb210.R

--- SedimentChronology_Workflow-main/RERCA/04_bayesian_pb210.R
+++ SedimentChronology_Workflow-main/RERCA/04_bayesian_pb210.R
@@ -57,7 +57,6 @@ rstan_options(auto_write = TRUE)
 # =============================================================================
 
 data_file <- "RERCA/data/example_pb210_data.csv"   # <-- CHANGE THIS: paste the full path to your CSV here, e.g.:
-# data_file <- "/Users/cathaldoherty/Desktop/CoastalBC_WorkflowV1/example_pb210_data.csv"
 
 core_raw <- read.csv(data_file, na.strings = c("NA", "")) %>%
   mutate(
